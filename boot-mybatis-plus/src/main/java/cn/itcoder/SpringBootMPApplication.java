21b267deab6456fe3077c366d2eaf440a22e3104
adminwei <1766990107>
2023-05-31 14:00:02 +0800

    【feat】：添加依赖

---
+++ boot-mybatis-plus/src/main/java/cn/itcoder/SpringBootMPApplication.java
@@ -0,0 +1,10 @@
+package cn.itcoder;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMPApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
